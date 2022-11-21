b63ca5daf9b81ccb8e7620146c56f66bc2e1d832
¨mago¨ <martingramajo08@gmail.com>
2022-11-21 11:49:47 -0300

    configuraciones base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import datos from './db.json';
+import NavReact from "./components/NavReact";
+import Home from "./pages/Home";
+import { ClienteLink } from "./pages/ClienteLink ";
+import Footer from "./components/Footer";
+import { DarkModeContext } from "./context/DarkModeContext";
+import { useContext } from "react";
 
 function App() {
+
+  /*Datos de la db.json*/
+  const [dato1, dato2] = datos
+
+  const { darkMode } = useContext(DarkModeContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? ' Container-dark' : 'Container-light '} style={{
+      display: "flex",
+      flexDirection: 'column',
+      minHeight: "100vh"
+    }}>
+      <BrowserRouter>
+        <NavReact />
+        <Routes >
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Cliente1" element={<ClienteLink dato={dato1} />} />
+          <Route exact path="/Cliente2" element={<ClienteLink dato={dato2} />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
